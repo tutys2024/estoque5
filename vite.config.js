628d543723696526a929f386cfcaf51de6df52b1
tutys2024 <luanbarretoandrade@hotmail.com>
2025-07-18 13:44:10 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/estoque5/', // Caminho correto para publicação no GitHub Pages
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
